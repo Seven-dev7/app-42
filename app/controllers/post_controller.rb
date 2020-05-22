1513e1a7fe40dc702e76f6615173ac3454c48f83
Se7endz <oussama.bendifallahd@gmail.com>
2020-05-22 16:43:16 +0200

    Add relation in db

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,21 @@
 class PostController < ApplicationController
+	def new
+		@post = Post.new
+	end
+
+	def show
+		@post = Post.all
+	end
+
+	def create
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/new'
  devise_for :users
+ resources :posts
 	root to: "home#index"
 end

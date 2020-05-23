21486d39f9292ca9751f0ed660ca8da81cbb5f9f
Se7endz <oussama.bendifallahd@gmail.com>
2020-05-23 18:28:30 +0200

    clean post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+
+	before_action :set_post, only: [:update, :edit, :destroy, :show]
 	
 	def index
 		@posts = Post.all
@@ -9,18 +11,34 @@ class PostsController < ApplicationController
 	end
 
 	def show
-		@post = Post.find(params[:id])
+	end
+
+	def edit
+		
 	end
 
 	def create
-		
+		post = Post.create(post_params)
+		redirect_to post_path(post.id)
 	end
 
 	def update
-		
+		@post.update(post_params)
+		redirect_to posts_path
 	end
 
 	def destroy
-		
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	private
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :content, :user_id)
 	end
 end

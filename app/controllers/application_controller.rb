69d5290986345bec348d23af60b5eab3939df9b4
Se7endz <oussama.bendifallahd@gmail.com>
2020-05-25 12:31:20 +0200

    add redirection to root_path when u try to acces to a denied page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'Accés Interdit' }
+      format.html { redirect_to root_path, notice: exception.message }
+      format.js   { head :forbidden, content_type: 'Accés Interdit' }
+    end
+  end
 end

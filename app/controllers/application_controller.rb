34d4f606ef8b4e69c7ee5e8ac4b8c9de088cbc38
Se7endz <oussama.bendifallahd@gmail.com>
2020-05-25 12:37:22 +0200

    better redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
-    respond_to do |format|
-      format.json { head :forbidden, content_type: 'Accés Interdit' }
-      format.html { redirect_to root_path, notice: exception.message }
-      format.js   { head :forbidden, content_type: 'Accés Interdit' }
-    end
+  	redirect_back fallback_location: { action: "show"}, :alert => "Accés Interdit"
   end
 end

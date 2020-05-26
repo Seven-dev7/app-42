a7d1517cbaa42147800690873ea2622cf33f01c6
Se7endz <oussama.bendifallahd@gmail.com>
2020-05-26 16:50:03 +0200

    change env2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['twitter_api_public'], ENV['twitter_api_secret']
+  provider :twitter, ENV['twitter_api_public'], ENV['twitter_api_secret'], ENV['twitter_api_tokken'], ENV['twitter_api_tokken_secret']
 end
